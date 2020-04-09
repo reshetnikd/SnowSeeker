150f4b93a3522e9f90e76d0ad997421692c55cb7
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-09 21:30:44 +0300

    Try out some code using groups as transparent layout containers

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,9 +8,71 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
+struct UserView: View {
     var body: some View {
-        Text("Hello, World!")
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna, Arya, and Toby")
+        }
+    }
+}
+
+struct ContentView: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+//    @State private var layoutVertically = false
+//    @State private var selectedUser: User? = nil
+//    @State private var isShowingAlert = false
+    
+    var body: some View {
+        Group {
+            if sizeClass == .compact {
+                VStack(content: UserView.init)
+            } else {
+                HStack(content: UserView.init)
+            }
+        }
+//        Group {
+//            if layoutVertically {
+//                VStack {
+//                    UserView()
+//                }
+//            } else {
+//                HStack {
+//                    UserView()
+//                }
+//            }
+//        }
+//        .onTapGesture {
+//            self.layoutVertically.toggle()
+//        }
+//        Text("Hello, World!")
+//            .onTapGesture {
+//                self.selectedUser = User()
+//                self.isShowingAlert = true
+//            }
+//            .alert(isPresented: $isShowingAlert) {
+//                Alert(title: Text(selectedUser!.id))
+//            }
+//        Text("Hello, World!")
+//            .onTapGesture {
+//                self.selectedUser = User()
+//            }
+//            .alert(item: $selectedUser) { user in
+//                Alert(title: Text(user.id))
+//            }
+//        NavigationView {
+//            NavigationLink(destination: Text("New secondary")) {
+//                Text("Hello, World!")
+//            }
+//            .navigationBarTitle("Primary")
+//
+//            Text("Secondary")
+//        }
     }
 }
 
