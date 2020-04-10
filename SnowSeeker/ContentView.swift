d4fe83ff04b24f8c4e64e1a9c9b810d5ddd9e183
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-10 22:03:05 +0300

    Build a primary list of items

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,71 +8,35 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
-}
-
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna, Arya, and Toby")
-        }
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-//    @State private var layoutVertically = false
-//    @State private var selectedUser: User? = nil
-//    @State private var isShowingAlert = false
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     var body: some View {
-        Group {
-            if sizeClass == .compact {
-                VStack(content: UserView.init)
-            } else {
-                HStack(content: UserView.init)
+        NavigationView {
+            List(resorts) { resort in
+                NavigationLink(destination: Text(resort.name)) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                    
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
             }
+            .navigationBarTitle("Resorts")
         }
-//        Group {
-//            if layoutVertically {
-//                VStack {
-//                    UserView()
-//                }
-//            } else {
-//                HStack {
-//                    UserView()
-//                }
-//            }
-//        }
-//        .onTapGesture {
-//            self.layoutVertically.toggle()
-//        }
-//        Text("Hello, World!")
-//            .onTapGesture {
-//                self.selectedUser = User()
-//                self.isShowingAlert = true
-//            }
-//            .alert(isPresented: $isShowingAlert) {
-//                Alert(title: Text(selectedUser!.id))
-//            }
-//        Text("Hello, World!")
-//            .onTapGesture {
-//                self.selectedUser = User()
-//            }
-//            .alert(item: $selectedUser) { user in
-//                Alert(title: Text(user.id))
-//            }
-//        NavigationView {
-//            NavigationLink(destination: Text("New secondary")) {
-//                Text("Hello, World!")
-//            }
-//            .navigationBarTitle("Primary")
-//
-//            Text("Secondary")
-//        }
     }
 }
 
---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,29 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Dmitry Reshetnik on 10.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+    
+//    Alternative
+//    static let example = (Bundle.main.decode("resorts.json") as [Resort])[0]
+}
